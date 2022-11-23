acc55a5516610064dd1ca8e8c7d25d9b802c3f47
shitingbao <871258317@qq.com>
2022-11-23 15:33:28 +0800

    perf: rust mod

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,5 +1,11 @@
+// mod 文件相当于一个当前包的引入包列表
+// 引入其他包
+
+// 这里的 client和garden 对应了一个他的目录和一个文件，文件内部定义并引入目录中的文件
 pub mod client;
 
+// 就是说 garden 目录内部的文件，要一个相同名字的 garden.rs 文件去引入目录中的文件
 pub mod garden;
 
+// 注意这个 mod 需要 pub 才能被外部使用
 pub mod test;
